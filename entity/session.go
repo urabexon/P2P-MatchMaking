81c16e302887bab5e2269cdd7f6b806c77d4fb4c
urabexon <hiroki708.a48uk@gmail.com>
2025-07-30 00:29:17 +0900

    add session func

--- entity/session.go
+++ entity/session.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -11,4 +12,28 @@ type Session[T comparable] struct {
 
 func NewSession[T comparable]() *Session[T] {
 	return &Session[T]{queue: make([]T, 0)}
+}
+
+func (s *Session[T]) Add(t T) {
+	s.Lock()
+	defer s.Unlock()
+	s.queue = append(s.queue, t)
+}
+
+func (s *Session[T]) Dequeue() (T, error) {
+	s.Lock()
+	defer s.Unlock()
+	if len(s.queue) == 0 {
+		var zeroT T
+		return zeroT, fmt.Errorf("queue is empty")
+	}
+	top := s.queue[0]
+	s.queue = s.queue[1:]
+	return top, nil
+}
+
+func (s *Session[T]) CanMatch() bool {
+	s.Lock()
+	defer s.Unlock()
+	return len(s.queue) >= 2
 }
