24bf8ec95b0838fd272a926d069265efca216ca1
urabexon <hiroki708.a48uk@gmail.com>
2025-07-30 00:07:29 +0900

    add user func

--- entity/user.go
+++ entity/user.go
@@ -15,3 +15,15 @@ type User struct {
 func NewUser(conn *websocket.Conn, id string, createdAt time.Time) *User {
 	return &User{conn, id, createdAt}
 }
+
+func (u *User) Conn() *websocket.Conn {
+	return u.conn
+}
+
+func (u *User) ID() string {
+	return u.id
+}
+
+func (u *User) CreatedAt() time.Time {
+	return u.createdAt
+}
